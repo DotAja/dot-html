e38e5be7df47c862037c01c5420ec7e538a5c73f
DotAja <142836390+DotAja@users.noreply.github.com>
2024-02-14 13:07:30 +0700

    Update cf.sh

--- cf.sh
+++ cf.sh
@@ -4,7 +4,7 @@ key="8ffca2a35ab4c6577c875232dda429b605cad"
 domen="toko.dotaja.my.id"
 ipena=$(curl -s ifconfig.me)
 
-curl -X PUT "https://api.cloudflare.com/client/v4/zones/your_zone_id/dns_records" \
+curl -X PUT "https://api.cloudflare.com/client/v4/zones/1479d9655befa35f9ad174401cf1ae2c/dns_records" \
      -H "X-Auth-Email: $imel" \
      -H "X-Auth-Key: $key" \
      -H "Content-Type: application/json" \
