216cd0785596fbe1210bc2ffaeccf342e5e44f29
DotAja <142836390+DotAja@users.noreply.github.com>
2024-02-14 14:38:41 +0700

    Update hrml.sh

--- hrml.sh
+++ hrml.sh
@@ -3,9 +3,11 @@ apt update
 apt upgrade -y
 apt install apache2 -y
 apt install certbot -y
+apt install unzip -y
 apt-get install certbot python3-certbot-apache -y
 chown -R www-data:www-data /var/www/html
 chmod -R 755 /var/www/html
+rm /var/www/html/index.html
 wget "https://github.com/DotAja/dot-html/raw/main/htmlku.zip" -O /tmp/file.zip
 unzip -o /tmp/file.zip -d "/var/www/html"
 rm /tmp/file.zip
