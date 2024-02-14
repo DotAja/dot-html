7604c0e306ab33110b7a8d610ef1c0dfebf6e2ba
DotAja <142836390+DotAja@users.noreply.github.com>
2024-02-14 13:18:04 +0700

    Update cf.sh

--- cf.sh
+++ cf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 imel="hendykusnandy@gmail.com"
 key="8ffca2a35ab4c6577c875232dda429b605cad"
-domen="toko.dotaja.my.id"
+domen="toko"
 ipena=$(curl -s ifconfig.me)
 
 curl -X PUT "https://api.cloudflare.com/client/v4/zones/1479d9655befa35f9ad174401cf1ae2c/dns_records" \
