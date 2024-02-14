748eee407d59ce3d2d03ca279d4c037b92256d78
DotAja <142836390+DotAja@users.noreply.github.com>
2024-02-14 12:53:18 +0700

    Add files via upload

---
+++ cf.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+imel="hendykusnandy@gmail.com"
+key="8ffca2a35ab4c6577c875232dda429b605cad"
+domen="toko.dotaja.my.id"
+ipena=$(curl -s ifconfig.me)
+
+curl -X PUT "https://api.cloudflare.com/client/v4/zones/your_zone_id/dns_records" \
+     -H "X-Auth-Email: $imel" \
+     -H "X-Auth-Key: $key" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"A","name":"'$domen'","content":"'$ipena'","ttl":1,"proxied":false}'
+
+certbot --apache --domain $domen
+certbot renew --quiet
