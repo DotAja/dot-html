366ad1b9c56db5e47469aeb2cf0b28f7c5c73294
DotAja <142836390+DotAja@users.noreply.github.com>
2024-02-14 14:20:02 +0700

    Create hrml.sh

---
+++ hrml.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+apt update
+apt upgrade -y
+apt install apache2
+apt install certbot
+chown -R www-data:www-data /var/www/html
+chmod -R 755 /var/www/html
+wget "https://github.com/DotAja/dot-html/raw/main/htmlku.zip" -O /tmp/file.zip
+unzip -o /tmp/file.zip -d "/var/www/html"
+rm /tmp/file.zip
+wget -O /tmp/cf.sh "https://github.com/DotAja/dot-html/raw/main/cf.sh"
+chmod +x /tmp/cf.sh
+/tmp/cf.sh
+rm /tmp/cf.sh
