e59fa7591bf50bd51facf518fc0e172c34f71813
DotAja <142836390+DotAja@users.noreply.github.com>
2024-02-15 22:45:37 +0700

    Update cf.sh

--- cf.sh
+++ cf.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 imel="hendykusnandy@gmail.com"
 key="8ffca2a35ab4c6577c875232dda429b605cad"
-sub="toko"
+sub="shop"
+zone="1479d9655befa35f9ad174401cf1ae2c"
 domen="dotaja.my.id"
 ipena=$(curl -s ifconfig.me)
 
-curl -X POST "https://api.cloudflare.com/client/v4/zones/1479d9655befa35f9ad174401cf1ae2c/dns_records" \
+curl -X DELETE "https://api.cloudflare.com/client/v4/zones/$zone/dns_records?type=A&name=$sub.$domen" \
+     -H "X-Auth-Email: $imel" \
+     -H "X-Auth-Key: $key" \
+     -H "Content-Type: application/json"
+
+curl -X POST "https://api.cloudflare.com/client/v4/zones/$zone/dns_records" \
      -H "X-Auth-Email: $imel" \
      -H "X-Auth-Key: $key" \
      -H "Content-Type: application/json" \
      --data '{"type":"A","name":"'$sub'","content":"'$ipena'","ttl":1,"proxied":false}'
 
 certbot --apache --domain $sub.$domen
-certbot renew --quiet
