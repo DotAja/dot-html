1049225dba6cbdd8fd2af466770ae9d35ff7b696
DotAja <142836390+DotAja@users.noreply.github.com>
2024-02-17 23:35:03 +0700

    Update hrml.sh

--- hrml.sh
+++ hrml.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+echo "Masukkan link ZIP dari situs web:"
+read webhtml
 apt update
 apt upgrade -y
 apt install apache2 -y
@@ -8,10 +11,10 @@ apt-get install certbot python3-certbot-apache -y
 chown -R www-data:www-data /var/www/html
 chmod -R 755 /var/www/html
 rm /var/www/html/index.html
-wget "https://github.com/DotAja/dot-html/raw/main/htmlku.zip" -O /tmp/file.zip
-unzip -o /tmp/file.zip -d "/var/www/html"
-rm /tmp/file.zip
-wget -O /tmp/cf.sh "https://github.com/DotAja/dot-html/raw/main/cf.sh"
-chmod +x /tmp/cf.sh
-/tmp/cf.sh
-rm /tmp/cf.sh
+wget -O /tmp/file.zip "$webhtml" && unzip -o /tmp/file.zip -d "/var/www/html" && rm /tmp/file.zip
+wget -O /tmp/cf.sh "https://github.com/DotAja/dot-html/raw/main/cf.sh" && chmod +x /tmp/cf.sh && /tmp/cf.sh && rm /tmp/cf.sh
+clear
+sleep 5
+====================================
+echo "https://shop.dotaja.my.id"
+====================================
